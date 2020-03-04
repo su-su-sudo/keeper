2cdeca7ce28a7bf773d78fd37823c007159674aa
Ian Aaberg <ian@Ians-MacBook-Pro.local>
2020-03-04 13:17:31 -0500

    added server.js

---
+++ build/precache-manifest.e2da45c98edaab9c3dd2c4392fafe083.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a8bf57c12b66146e3fb1f0316f5cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef55678816a43782b70",
+    "url": "/static/js/2.f0bd724e.chunk.js"
+  },
+  {
+    "revision": "f0b79583ce9dacd39879",
+    "url": "/static/js/main.21b2f8e0.chunk.js"
+  },
+  {
+    "revision": "bb3087a6f23f5a439dbd",
+    "url": "/static/js/runtime-main.5fc0b959.js"
+  }
+]);
